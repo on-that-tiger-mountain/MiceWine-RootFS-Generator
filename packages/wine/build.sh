5262cddc14ca5fc41f65de44fe22471722e77716
on-that-tiger-mountain <167862084+on-that-tiger-mountain@users.noreply.github.com>
2025-10-02 15:20:08 +0800

    update build.sh

--- packages/wine/build.sh
+++ packages/wine/build.sh
@@ -4,8 +4,8 @@ PKG_PRETTY_NAME="Wine ($PKG_VER)"
 
 BLACKLIST_ARCH=aarch64
 
-GIT_URL=https://github.com/KreitinnSoftware/wine
-GIT_COMMIT=878fcd8452d4f27b634b0db802469a738d37270d
+GIT_URL=https://github.com/on-that-tiger-mountain/wine
+GIT_COMMIT=0852ea6e4970ce1ede76ba954bd91e9aa4920665
 HOST_BUILD_CONFIGURE_ARGS="--enable-win64 --without-x"
 HOST_BUILD_FOLDER="$INIT_DIR/workdir/$package/wine-tools"
 HOST_BUILD_MAKE="make -j $(nproc) __tooldeps__ nls/all"
